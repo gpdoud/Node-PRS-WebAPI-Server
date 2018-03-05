01c668a0790c47160e73acf202472621a996f047
Greg doud <gpdoud@gmail.com>
2018-03-05 16:41:11 -0500

    Possible bug in server.js

--- js/user.service.js
+++ js/user.service.js
@@ -1,32 +1,56 @@
 $().ready(function() {
 
-    function add(user) {
+    function get(id) {
+        $.getJSON("http://localhost:5000/Users/Get/"+id)
+          .done(function(res) {
+            display(res.data);
+            console.log(res);
+          })
+          .fail(function(err) {
+            console.error(err);
+          });
+    }
 
-        $.post("http://localhost:5000/User/Create", user)
+    function display(user) {
+        $("#idx").val(user.Id);
+        $("#firstname").val(user.FirstName);
+        $("#lastname").val(user.LastName);
+        $("#username").val(user.UserName);
+        $("#password").val(user.Password);
+        $("#phone").val(user.Phone);
+        $("#email").val(user.Email);
+        $("#reviewer").prop("checked", user.IsReviewer);
+        $("#admin").prop("checked", user.IsAdmin);
+    }
+
+    $("#getId").click(function() {
+      var id = $("#theId").val();
+      get(id);
+    });
+
+    function change(user) {
+        $.post("http://localhost:5000/Users/Change", user)
             .done(function(res) {
               console.log(res);
             })
             .fail(function(err) {
               console.error(err);
             });
-
     }
 
-    $("#add").click(function() {
-
+    $("#chg").click(function() {
         var user = {
-          Id: 0,
+          Id: $("#idx").val(),
           FirstName: $("#firstname").val(),
           LastName: $("#lastname").val(),
           UserName: $("#username").val(),
           Password: $("#password").val(),
           Phone: $("#phone").val(),
           Email: $("#email").val(),
-          IsReviewer: $("#reviewer").checked(),
-          IsAdmin: $("#admin").checked()
+          IsReviewer: $("#reviewer").prop("checked"),
+          IsAdmin: $("#admin").prop("checked")
         };
-        add(user);
-
+        change(user);
     });
 
 });
